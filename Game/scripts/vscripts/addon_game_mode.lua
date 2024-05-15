e413bcb30f03c063083aaa359a0c781f6c97ab67
avtakhov <avtahovfarit@gmail.com>
2024-05-15 18:19:04 +0300

    xp think

--- Game/scripts/vscripts/addon_game_mode.lua
+++ Game/scripts/vscripts/addon_game_mode.lua
@@ -2,6 +2,7 @@ require ("timers")
 require ("game_settings")
 require ("gamemode")
 require ("item_drop")
+require ("game_managers/config")
 
 function Precache( context )
 	--[[
@@ -18,27 +19,15 @@ end
 
 -- Create the game mode when we activate
 function Activate()
-GameRules.AddonTemplate = CAddonTemplateGameMode()
-GameRules.AddonTemplate:InitGameMode()
+	GameRules.AddonTemplate = CAddonTemplateGameMode()
+	GameRules.AddonTemplate:InitGameMode()
 end
+
+CAddonTemplateGameMode = CAddonTemplateGameMode or class({})
 
 function CAddonTemplateGameMode:InitGameMode()
-print( "Template addon is loaded." )
-GameRules:GetGameModeEntity():SetThink( "OnThink", self, "GlobalThink", 2 )
-end
-
--- Evaluate the state of the game
-function CAddonTemplateGameMode:OnThink()
-if GameRules:State_Get() == DOTA_GAMERULES_STATE_GAME_IN_PROGRESS then
---print( "Template addon script is running." )
-elseif GameRules:State_Get() >= DOTA_GAMERULES_STATE_POST_GAME then
-return nil
-end
-return 1
-end
-
-function CAddonTemplateGameMode:InitGameMode()
-GameRules:GetGameModeEntity():SetFreeCourierModeEnabled(true)
-GameRules:SetGoldTickTime(1)
-GameRules:SetGoldPerTick(2)
+	GameRules:GetGameModeEntity():SetFreeCourierModeEnabled(true)
+	GameRules:SetGoldTickTime(1)
+	GameRules:SetGoldPerTick(2)
+	InitGameManagers()
 end
